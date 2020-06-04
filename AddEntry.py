9758a7d6129787cc9398c3d689c1bf2561ab955a
Tenesh Vignesan <teneshbecs@gmail.com>
2020-06-05 02:55:27 +0800

    Updated new entry file with 1. Added validation for empty fields 2. Added popup methods 3. Added saving data to db 4. Modified saving data by fetching id of part name from part table

--- AddEntry.py
+++ AddEntry.py
@@ -15,6 +15,13 @@ class AddEntryApp(QDialog, addentry_ui):
         super(AddEntryApp, self).__init__(parent)
         self.setupUi(self)
         self.ShowEmployee_custom()
+        self.ShowName_part()
+        self.HandleButtonAction()
+
+    # Method for Button action
+    def HandleButtonAction(self):
+        self.pushButtonNewEntryAdd.clicked.connect(self.AddEntry_validate)
+        self.pushButtonNewEntryReset.clicked.connect(self.AddEntry_reset)
 
     # Method to show drop-down data
     def ShowEmployee_custom(self):
@@ -32,5 +39,77 @@ class AddEntryApp(QDialog, addentry_ui):
         self.cur.execute('SELECT part_name FROM part')
         part_list = self.cur.fetchall()
         for part in part_list:
-            self.comboBoxNewEntry.addItem(part[0])
+            self.comboBoxNewEntryPart.addItem(part[0])
         self.db.close()
+
+    # Method to validate empty field data entry
+    def AddEntry_validate(self):
+        if not self.lineNewEntryDescription.toPlainText() == '':
+            self.AddEntry_Data()
+        else:
+            self.error_popup("Input Error", "Invalid input or empty Description field")
+
+    # Method to reset all add part fields
+    def AddEntry_reset(self):
+        self.comboBoxNewEntryPart.clear()
+        self.comboBoxNewEntryEmployee.clear()
+        self.spinBoxNewEntryQuantity.clear()
+        self.lineNewEntryDescription.clear()
+        self.comboBoxNewEntryEmployee.clear()
+        self.spinBoxNewEntryQuantity.setValue(1)
+        self.ShowName_part()
+        self.ShowEmployee_custom()
+
+    # Method to add entry data
+    def AddEntry_Data(self):
+        self.db = ConnectDatabase()
+        part_id = self.comboBoxNewEntryPart.currentText()
+        entry_employee = self.comboBoxNewEntryEmployee.currentText()
+        entry_status = self.comboBoxNewEntryStatus.currentText()
+        entry_quantity = self.spinBoxNewEntryQuantity.value()
+        entry_description = self.lineNewEntryDescription.toPlainText()
+        entry_date = self.dateNewEntryDate.text()
+
+        try:
+            self.cur = self.db.cursor()
+            self.cur.execute('SAVEPOINT SP1')
+            self.cur.execute('''SELECT id FROM part WHERE part_name = %s ''', [(part_id,)])
+        except Exception as error:
+            self.cur.execute('ROLLBACK TO SAVEPOINT SP1')
+            self.AddEntry_reset()
+            self.error_popup("Input Error", error)
+        else:
+            fetch_id = self.cur.fetchone()
+            try:
+                self.cur.execute('''INSERT INTO entry (part_id, entry_status, entry_qty, entry_desc, entry_date, entry_employee)
+                                                    VALUES (%s , %s , %s , %s , %s , %s)''', (fetch_id, entry_status, entry_quantity, entry_description, entry_date, entry_employee))
+            except Exception as error:
+                self.cur.execute('ROLLBACK TO SAVEPOINT SP1')
+                self.AddEntry_reset()
+                self.error_popup("Input Error", error)
+            else:
+                self.cur.execute('RELEASE SAVEPOINT SP1')
+                self.db.commit()
+                self.AddEntry_reset()
+                self.success_popup("Entry", "New Entry Added.")
+        self.db.close()
+
+    # Method to show success popup window
+    def success_popup(self, title_popup, msg_popup):
+        popup = QMessageBox()
+        popup.setFixedSize(500, 500)
+        popup.setWindowTitle(title_popup)
+        popup.setIcon(QMessageBox.Information)
+        popup.setStyleSheet("font:9pt Poppins;")
+        popup.setText(msg_popup)
+        popup.exec_()
+
+    # Method to show error popup window
+    def error_popup(self, title_popup, msg_popup):
+        popup = QMessageBox()
+        popup.setFixedSize(500, 500)
+        popup.setWindowTitle(title_popup)
+        popup.setIcon(QMessageBox.Warning)
+        popup.setStyleSheet("font:9pt Poppins;")
+        popup.setText(msg_popup)
+        popup.exec_()
